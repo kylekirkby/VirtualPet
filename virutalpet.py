7dd0820e713c8c25ffb613cd9deb5eb58d1087da
Kyle Kirkby <kylekirkby@googlemail.com>
2014-06-11 09:50:14 +0100

    First OOP Example in A2 Computing

---
+++ virutalpet.py
@@ -0,0 +1,18 @@
+class VirtualPet:
+
+    """ A virtual pet which can talk and do other things"""
+
+    def talk(self):
+        print("Hello, I am your new virtual pet.")
+
+
+def main():
+
+    pet_one = VirtualPet()
+    pet_one.talk()
+
+
+
+if __name__ == "__main__":
+
+    main()
