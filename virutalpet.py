92ab3ac93283d7eb34262672a32bf9e804559c2f
Kyle Kirkby <kylekirkby@googlemail.com>
2014-06-11 10:04:57 +0100

    Added attributes and passed in a couple of params

--- virutalpet.py
+++ virutalpet.py
@@ -1,17 +1,36 @@
 class VirtualPet:
 
     """ A virtual pet which can talk and do other things"""
+    
+    def __init__ (self,name):
+        
+        print("Hi i have been born")
 
+        #attributes
+        
+        self.name = name
+        
     def talk(self):
-        print("Hello, I am your new virtual pet.")
 
+        print("Hello, I am {0}.".format(self.name))
 
+    def eat(self, food):
+
+        if food == "apple":
+            print("I love apples!")
+        elif food == "cake":
+            print("Cake is awesome!")
+        elif food == "sandwich":
+            print("Nothing like a good sandwich!")
+        else:
+            print("I don't like that food!")
+        
 def main():
-
-    pet_one = VirtualPet()
+    name = input("PLease enter a name for your pet: ")
+    pet_one = VirtualPet(name)
     pet_one.talk()
-
-
+    food = input("I'm hungry. What can you give me?: ")
+    pet_one.eat(food)
 
 if __name__ == "__main__":
 
