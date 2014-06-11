731159ef49912701fea573fbdb1e0ba7e04f30dd
Kyle Kirkby <kylekirkby@googlemail.com>
2014-06-11 10:27:00 +0100

    Added ability to eat

--- virutalpet.py
+++ virutalpet.py
@@ -1,3 +1,5 @@
+import random
+
 class VirtualPet:
 
     """ A virtual pet which can talk and do other things"""
@@ -9,28 +11,47 @@ class VirtualPet:
         #attributes
         
         self.name = name
+        self.hunger = 10
+        self.energy = 1
         
-    def talk(self):
+    def talk(self, speech):
 
-        print("Hello, I am {0}.".format(self.name))
+        print(speech)
 
     def eat(self, food):
 
         if food == "apple":
+            self.hunger = self.hunger - 2
+            self.energy = self.energy + 2
             print("I love apples!")
+            
         elif food == "cake":
+            self.hunger = self.hunger - 4
+            self.energy = self.energy + 4
             print("Cake is awesome!")
+            
         elif food == "sandwich":
+            self.hunger = self.hunger - 3
+            self.energy = self.energy + 3
             print("Nothing like a good sandwich!")
+            
         else:
             print("I don't like that food!")
+
+        print("New energy level: {0}".format(self.energy))
+        print("New hunger level: {0}".format(self.hunger))
         
 def main():
-    name = input("PLease enter a name for your pet: ")
+    name = input("Please enter a name for your pet: ")
     pet_one = VirtualPet(name)
-    pet_one.talk()
-    food = input("I'm hungry. What can you give me?: ")
-    pet_one.eat(food)
+    pet_one.talk("Hi i am {0}".format(name))
+
+    while pet_one.hunger > 5:
+        sayings = ["I'm still hungry!", "My tummy is rumbling","Give me more food! I need energy!"]
+        randomNum = random.randint(0,2)
+        pet_one.talk(sayings[randomNum])
+        food = input("Enter food name:")
+        pet_one.eat(food)
 
 if __name__ == "__main__":
 
